7fd45ae228080991e64776142afbd6b7562e6e4a
Iwmmeng <834638005@qq.com>
2019-06-24 17:22:51 +0800

    update tableName and pom dependcy

--- src/main/java/com/xiaomi/rnstringweb/entities/KeyInfo.java
+++ src/main/java/com/xiaomi/rnstringweb/entities/KeyInfo.java
@@ -8,10 +8,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.util.Date;
 
 @Entity
-@Table(name = "key_info")
+@Table(name = "keyInfo")
 public class KeyInfo {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
